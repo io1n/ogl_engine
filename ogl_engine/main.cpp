6f94b389624371f86775ef2f34e30ca34c544a06
io1n <ontheedge2002@gmail.com>
2021-01-19 23:14:50 +0700

    1

--- ogl_engine/main.cpp
+++ ogl_engine/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
+#include "render/VBO.h"
+
 #ifdef _DEBUG
 int main()
 #else
 int WinMain()
 #endif
 {
+	glfwInit();
+
+	printf("%f\n", glfwGetTime());
+
+	glfwTerminate();
 
 	return 0;
 }
--- ogl_engine/render/VBO.h
+++ ogl_engine/render/VBO.h
@@ -1,6 +1,9 @@
 #ifndef VBO_H
 #define	VBO_H
 
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
 class VBO {
 
 };
